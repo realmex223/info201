134988bbef6639cc9dfc74db53b986887535da02
realmex223 <146480677+realmex223@users.noreply.github.com>
2023-12-09 17:38:43 -0800

    added chloroplethy map Final_pro.R

--- Final_pro.R
+++ Final_pro.R
@@ -1,6 +1,10 @@
 library(dplyr)
 library(stringr)
 library(ggplot2)
+library(plotly)
+library(tidyverse)
+library(urbnmapr)
+
 
 pvr_df<- read.csv("hate_crimes.csv")
 min_wage_df <- read.csv("MinimumWageData.csv") 
@@ -13,3 +17,34 @@ merged_df$income_category <- ifelse(merged_df$median_household_income > median(m
 merged_df$wage_income_comparison <- merged_df$median_household_income/ merged_df$State.Minimum.Wage
  
 unemployment_df <- aggregate(cbind(median_household_income, share_unemployed_seasonal) ~ state, data = merged_df, FUN = mean)
+
+
+#this is the file that were going to do all of our data wrangling on 
+
+#story one 
+#Chloroplethy map
+
+empty_graph<- ggplot() + 
+  geom_polygon(data = urbnmapr::states, mapping = aes(x = long, y = lat, group = group),
+               fill = "grey", color = "white") +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45)
+plot(empty_graph)
+
+state_data <- data.frame(
+  state = tolower(merged_df$state),  # Convert state names to lowercase
+  value = merged_df$median_household_income  # Replace with your actual data
+)
+
+# Download state boundaries data from the maps package
+state_map <- map_data("state")
+
+# Merge state data with state boundaries
+merged_data <- merge(state_map, state_data, by.x = "region", by.y = "state")
+
+# Create a ggplot object
+ggplot(merged_data, aes(x = long, y = lat, group = group, fill = value)) +
+  geom_polygon(color = "white") +
+  coord_map() +
+  scale_fill_gradient(name = "Value") +
+  theme_minimal()
+
