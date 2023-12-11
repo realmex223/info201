f5799f71e2ce048f0598425952ac0b6fa192c2eb
realmex223 <146480677+realmex223@users.noreply.github.com>
2023-12-10 23:15:24 -0800

    Update Final_pro.R

--- Final_pro.R
+++ Final_pro.R
@@ -5,7 +5,6 @@ library(plotly)
 library(tidyverse)
 library(urbnmapr)
 
-
 pvr_df<- read.csv("hate_crimes.csv")
 min_wage_df <- read.csv("MinimumWageData.csv") 
 
@@ -17,29 +16,7 @@ merged_df$income_category <- ifelse(merged_df$median_household_income > median(m
 merged_df$wage_income_comparison <- merged_df$median_household_income/ merged_df$State.Minimum.Wage
  
 unemployment_df <- aggregate(cbind(median_household_income, share_unemployed_seasonal) ~ state, data = merged_df, FUN = mean)
-
 merged_df$share_white <- 1 - merged_df$share_non_white
-
-merged_df$state <- tolower(merged_df$state)
-
-merged_df$state <- tolower(merged_df$state)
-
-# Create a new column with abbreviated state names
-merged_df$state_abbr <- state.abb[match(merged_df$state, tolower(state.name))]
-
-merged_df$state_abbr[is.na(merged_df$state_abbr)] <- "D.C"
-
-
-#this is the file that were going to do all of our data wrangling on 
-
-#story one 
-#Chloroplethy map
-
-empty_graph<- ggplot() + 
-  geom_polygon(data = urbnmapr::states, mapping = aes(x = long, y = lat, group = group),
-               fill = "grey", color = "white") +
-  coord_map(projection = "albers", lat0 = 39, lat1 = 45)
-plot(empty_graph)
 
 state_data <- data.frame(
   state = tolower(merged_df$state),  # Convert state names to lowercase
@@ -56,6 +33,27 @@ merged_data <- merge(state_map, state_data, by.x = "region", by.y = "state")
 ggplot(merged_data, aes(x = long, y = lat, group = group, fill = value)) +
   geom_polygon(color = "white") +
   coord_map() +
-  scale_fill_gradient(name = "Value") +
+  scale_fill_gradient(name = "Median Household Income",low = "lightblue", high = "darkblue" ) +
   theme_minimal()
+
+merged_df$state <- tolower(merged_df$state)
+
+# Create a new column with abbreviated state names
+merged_df$state_abbr <- state.abb[match(merged_df$state, tolower(state.name))]
+
+merged_df$state_abbr[is.na(merged_df$state_abbr)] <- "D.C"
+
+
+# Create the final plot with state abbreviations and a nicer theme
+final_plot <- ggplot(merged_df, aes(x = median_household_income, y = share_population_with_high_school_degree, label = state_abbr)) +
+  geom_point(color = "darkblue", size = 3, alpha = 0.7) +
+  geom_text(vjust = -0.5, hjust = 1, size = 3, color = "black") +
+  labs(title = "Median Household Income vs. Share of Population with High School Degree",
+       x = "Median Household Income",
+       y = "Share Population with High School Degree") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        axis.title = element_text(size = 14),
+        axis.text = element_text(size = 12),
+        legend.position = "none")
 
